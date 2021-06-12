7f1960712d666531b7baa95b1873555a581cce72
mr-crypt <pilakkatdheeraj@gmail.com>
2021-06-12 19:05:35 +0530

    fin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todoapp from './components/Todoapp'
+import Footer from './components/footer'
+function App() {
+    return (
+        <div>
+            <Todoapp/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Todoapp.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import "./Todoapp.css"
+export class Todoapp extends Component {
+    state = {
+        input:"",
+        items:[]
+    };
+    handleChange = event =>{
+        this.setState({
+            input: event.target.value
+        });
+    };
+    storeItems =event=>{
+        event.preventDefault();
+        const { input } = this.state;
+
+
+        this.setState({
+            items: [...this.state.items, input],
+            input: ""
+        });
+    };
+    deleteItem =(key)=>{
+        // const allItems= this.state.items;
+
+        this.state.items.splice(key,1);
+        console.log(key);
+        this.setState({
+            items:this.state.items.filter((index) => index !== key)
+            
+        })
+    };
+    deleteAll =() =>{
+        // const everyItem = this.state.items;
+        // everyItem.splice(key);
+        this.setState({
+            items:[]
+            // items:everyItem
+        })
+    }
+
+    render() {
+        const {input, items} =this.state;
+        return (
+            <div className="todo-container">
+
+                <form className="input-section" onSubmit={this.storeItems}>
+                    <h1>Todo App</h1>
+                    <div><input type="text" value={this.state.input} onChange={this.handleChange} placeholder="Enter items..."/>
+                    <button onClick={this.storeItems}>Add</button>
+                    </div>
+                </form>
+
+                    <ul>
+                        {items.map((data,index)=>(
+                            <div>
+                            <li key={index  }>{index}, {data} <i className="fas fa-trash-alt" onClick={()=>this.deleteItem(index)}></i></li>
+                            </div>
+                            )
+                        )}
+                            <button className="delete" onClick={()=>this.deleteAll()}>Delete All</button>
+                    </ul>
+            </div>
+        )
+    }
+}
+
+export default Todoapp
